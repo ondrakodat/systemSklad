2559edbe725c989655c900de39ccaf20bb829e40
ondrakodat12 <ondrej.kodat@seznam.cz>
2026-09-03 18:08:58 +0200

    Pridani cotrolleru pro Sklad

--- api/src/main/java/com/kodat/skladovysystem/controller/SkladController.java
+++ api/src/main/java/com/kodat/skladovysystem/controller/SkladController.java
@@ -1,4 +1,49 @@
 package com.kodat.skladovysystem.controller;
 
+import com.kodat.skladovysystem.Dto.SkladDto;
+import com.kodat.skladovysystem.entities.Sklad;
+import com.kodat.skladovysystem.interfaces.Iservices.ISkladService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/api/sklad")
 public class SkladController {
+    private final ISkladService _ISkladService;
+
+    public SkladController(ISkladService iSkladService) {
+        _ISkladService = iSkladService;
+    }
+
+    @GetMapping
+    public List<SkladDto> dejSklady(){
+        return _ISkladService.dejSklady();
+    }
+
+    @GetMapping("/{id}")
+    public SkladDto dejSkladPodleId(
+           @PathVariable long id){
+        return _ISkladService.dejSkladPodleId(id);
+    }
+
+    @PutMapping("/{id}")
+    public void upravSkladDtoPodleId(
+            @PathVariable long id,
+            @RequestBody SkladDto dto){
+        _ISkladService.upravSkladPodleID(dto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void smazSkladDtoPodleId(
+           @PathVariable long id){
+        _ISkladService.odeberSkladPodleId(id);
+    }
+
+    @PostMapping
+    public void pridejSkladDto(
+           @RequestBody SkladDto dto){
+        _ISkladService.pridejSklad(dto);
+    }
+
+
 }
